6f144a3ed44c2a7329979c71f0da1bdc069e3da4
samurang87 <samurang87@users.noreply.github.com>
2019-08-25 17:30:17 +0200

    add keyword arg

--- py_fashion_ml.py
+++ py_fashion_ml.py
@@ -3,11 +3,16 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
 
 print("Loading data...")
-train_data = np.loadtxt(fname='data/fashion-mnist_train.csv', delimiter=',', skiprows=1)
-test_data = np.loadtxt(fname='data/fashion-mnist_testsmall.csv', delimiter=',', skiprows=1)
+train_data = np.loadtxt(fname='data/fashion-mnist_train.csv',
+                        delimiter=',',
+                        skiprows=1)
+test_data = np.loadtxt(fname='data/fashion-mnist_testsmall.csv',
+                       delimiter=',',
+                       skiprows=1)
 print("Very data, much mnist, wow")
 
-neigh = KNeighborsClassifier(n_neighbors=5, algorithm='kd_tree')  # euclidean is the default distance
+# euclidean is the default distance
+neigh = KNeighborsClassifier(n_neighbors=5, algorithm='kd_tree')
 
 print("Many training...")
 neigh.fit(train_data[:, :-1], train_data[:, -1])
@@ -16,4 +21,4 @@ print("Very predict...")
 prediction = neigh.predict(test_data[:, :-1])
 
 print("Classification summary")
-print(classification_report(prediction, test_data[:, -1]))
+print(classification_report(y_pred=prediction, y_true=test_data[:, -1]))
